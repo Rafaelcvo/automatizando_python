1bcf39c7bde4f8c90bf477cdad0a9e83bb2b4f8d
Rafael <rafaelmoreiracvo@yahoo.com.br>
2021-04-29 16:26:17 -0300

    funcinonado no windows

--- teste.py
+++ teste.py
@@ -2,16 +2,20 @@ import pyautogui as auto
 import pywhatkit
 import time
 import pandas as pd
+import emoji
 
 cliente = pd.read_excel("Clientes.xlsx", engine='openpyxl')
-cliente['Telefone'] = pd.to_numeric(cliente['Telefone'])
-
 
 for index, row in cliente.iterrows():
+    row['Telefone'] ='+55' + str(row['Telefone'])
     h = int(time.strftime('%H', time.localtime()))
     m = int(time.strftime('%M', time.localtime()))
     m = m+1
-    msg = "Olá " + row['Nome'] + "tudo bem com você?"
+    msg = "Olá " + row['Nome'] + " tudo bem com você? " + \
+          "\nÉ possível inserir texto passando uma promoção." + \
+          '\n' + emoji.emojize("Podemos inserir emojis :red_heart:") + "." + \
+          '\n\n' + "Inserir links https://www.instagram.com/rafaelcvo/"
+
     pywhatkit.sendwhatmsg(row['Telefone'], msg, h, m)
     auto.PAUSE = 5
     auto.moveTo(1350,15)
